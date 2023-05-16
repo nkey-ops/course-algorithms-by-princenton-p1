89e347a19fc6679b65e2c6e0c62f1dcc34c9b2a1
deuru <terrythebear746@gmail.com>
2023-05-16 21:52:23 +0400

    Modified: Elementary Sorts 2.1 | Web Exercises | "DutchNationalFlag"

--- elementary_sorts_2_1/intquest/DutchNationalFlag.java
+++ elementary_sorts_2_1/intquest/DutchNationalFlag.java
@@ -6,26 +6,29 @@ import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 
 /**
- * @author Admin
- * Dutch national flag. 
- * Given an array of n buckets, 
- *  each containing a red, white, or blue pebble, sort them by color. 
- * The allowed operations are: 
+ * @author Admin Dutch national flag. Given an array of n buckets, each
+ *         containing a red, white, or blue pebble, sort them by color. The
+ *         allowed operations are:
  * 
- * swap(i,j):  swap the pebble in bucket i with the pebble in bucket j.
- * color(i): determine the color of the pebble in bucket i. 
+ *         swap(i,j): swap the pebble in bucket i with the pebble in bucket j.
+ *         color(i): determine the color of the pebble in bucket i.
  * 
- * The performance requirements are as follows: n calls to color().
- * At most n calls to swap().
- * Constant extra space.
+ *         The performance requirements are as follows: n calls to color(). At
+ *         most n calls to swap(). Constant extra space.
  */
 public class DutchNationalFlag {
-	private int[] buckets;
-	
+	private char[] buckets;
+
 	public static void main(String[] args) {
-		int[] buckets = new int[20];
+		char[] buckets = new char[20];
 		for (int i = 0; i < buckets.length; i++) {
-			buckets[i] = StdRandom.uniformInt(1, 4);
+			buckets[i] = 
+					switch(StdRandom.uniformInt(1, 4)) {
+					case(1) -> 'R';
+					case(2) -> 'W';
+					default -> 'B'; 
+			};
+				
 		}
 
 		DutchNationalFlag d =  new DutchNationalFlag();
@@ -36,30 +39,32 @@ public class DutchNationalFlag {
 		StdOut.println(Arrays.toString(d.buckets));
 	}
 
-	
 	/**
 	 * Sorts {1, 2, 3} integers using 3-way-partitioning
-	 *  
-	 * Time Complexity O(N)
-	 * Auxiliary Space O(1)
+	 * 
+	 * Time Complexity O(N) Auxiliary Space O(1)
 	 * 
 	 * @param a
 	 */
 	public void sort() {
-	//		index of checked colors
+		// index of checked colors
 		int lo = 0, hi = buckets.length - 1;
 
 		for (int i = 0; i <= hi; i++) {
-			int v = color(i); 
-			if(v == 1) swap(i, lo++);
-			else if(v == 3) swap(i--, hi--);
-		}	
+			char v = color(i);
+			if (v == 'W')
+				swap(i, lo++);
+			else if (v == 'R')
+				swap(i--, hi--);
+		}
 	}
 
-	private int color(int i) { return buckets[i] ;}
+	private char color(int i) {
+		return buckets[i];
+	}
 
 	private void swap(int i, int j) {
-		int tmp = buckets[i]; 
+		char tmp = buckets[i];
 		buckets[i] = buckets[j];
 		buckets[j] = tmp;
 	}
